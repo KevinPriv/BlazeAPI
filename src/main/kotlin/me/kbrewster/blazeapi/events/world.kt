fb3729081aada2c8830c503127a04fc537cce959
FalseHonesty <skipboman0@gmail.com>
2018-11-04 22:41:16 -0500

    Added and changed events

--- src/main/kotlin/me/kbrewster/blazeapi/events/world.kt
+++ src/main/kotlin/me/kbrewster/blazeapi/events/world.kt
@@ -4,6 +4,8 @@ import me.kbrewster.eventbus.AbstractEvent
 import net.minecraft.client.multiplayer.WorldClient
 
 /**
- * Fired once the world has been loaded
+ * Fired once the world has finished loading
  */
 class WorldLoadEvent(val worldClient: WorldClient, val loadingMessage: String) : AbstractEvent()
+
+class WorldUnloadEvent : AbstractEvent()
